d8def6c0ff49ffcfb15abec8e7c46265881ac3f3
Matteo <10934076@itisrossi.vi.it>
2025-11-28 11:43:16 +0100

    prova

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $title = "Accesso";
     </div>
 
     <div>
-        <label for="codice_fiscale">Codice Fiscale (opzionale)</label><br>
+        <label for="codice_fiscale">Codice Fiscale</label><br>
         <input type="text" id="codice_fiscale" name="codice_fiscale" placeholder="Inserisci solo se lo conosci">
     </div>
 
